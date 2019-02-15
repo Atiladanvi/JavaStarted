d2c3e578be4a7d83e27708711318d19c36808051
Atiladanvi <atila.danvi@outlook.com>
2019-02-15 20:26:10 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ *  Author: Atila Silva
+ *  Email: atila.danvi@outlook.com
+ *  Project: Janela Única
+ *  Class name: Main
+ *  made at 15/2/2019
+ *  Copyright (c) 2019.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Started !");
+    }
+}
