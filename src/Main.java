670d26f3c64db953c2976629f2f4a749bba5ea94
Atiladanvi <atila.danvi@outlook.com>
2019-02-17 21:41:18 -0300

    Wrapper run

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Wrapper.integer(2);
+        Wrapper.bool(true);
+        Wrapper.string("Hello World ");
+        Wrapper.character('H');
         System.out.println(Calc.pow(7.2 , 3));
         System.out.println(Calc.pow(2.0));
         System.out.println(Calc.sum(2.4 , 4.5));
         System.out.println(Calc.sum(2.92 , 4.4));
-
     }
 
 
---
+++ src/Wrapper.java
@@ -0,0 +1,30 @@
+class Wrapper {
+    /**
+     * Print value
+     * @param s Integer
+     */
+    static void integer(Integer s){
+       System.out.println(s);
+    }
+    /**
+     * Print value
+     * @param s String
+     */
+    static void string(String s){
+        System.out.println(s);
+    }
+    /**
+     * Print value
+     * @param s Character
+     */
+    static void character(Character s){
+        System.out.println(s);
+    }
+    /**
+     * Print value
+     * @param s Boolean
+     */
+    static void bool(Boolean s){
+        System.out.println(s);
+    }
+}
